5738f7b69ef672c10746ce242f5b80a8e29a2f95
Ing. Omar Sanchez <ing_omarglez@hotmail.com>
2023-10-04 17:39:48 -0600

    Creando database autores
    Creando database autores

---
+++ sentencias.sql
@@ -0,0 +1,25 @@
+/*
+Que tipo de entidades? autores
+Nombre : autores
+*/
+
+
+Nombre
+Genero
+Fecha de nacimiento
+País de Origen
+
+columna y el tipo de dato
+
+CREATE TABLE autores(
+    autor_id INT,
+    nombre VARCHAR(25),
+    apellido VARCHAR(25),
+    genero CHAR(1),
+    fecha_nacimiento DATE
+    pais_origen VARCHAR(40)
+);
+
+
+
+
